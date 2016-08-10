55999fdac6c614960cb29d39e4fcc2ffad02dddf
Sarang ITM <sarang@itmystix.com>
2016-08-10 14:25:45 +0530

    Added custom DateTime validation cods

--- CustomDateTimeValidation.cs
+++ CustomDateTimeValidation.cs
@@ -1,1 +1,18 @@
-
+public class DateTimeValidator : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null) return false;
+            try
+            {
+                DateTime dateTime;
+                var isValid = DateTime.TryParseExact(Convert.ToString(value), "dd/MM/yyyy", CultureInfo.CurrentCulture,
+                    DateTimeStyles.None, out dateTime);
+                return base.IsValid(isValid);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
