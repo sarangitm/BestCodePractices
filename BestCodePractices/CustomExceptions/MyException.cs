458fdc32e8df81e5c45d6783c9e5b145ce51d4be
SarangK <sarang@itmystix.com>
2017-08-31 20:23:59 +0530

    Added Custom Exception

---
+++ BestCodePractices/CustomExceptions/MyException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestCodePractices.CustomExceptions
+{
+    public class MyException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public MyException()
+        {
+            // Do your stuff
+        }
+
+        public MyException(int statusCode)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public MyException(int statusCode, string errorMessage)
+            : base(errorMessage)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public MyException(int statusCode, Exception exception)
+            : base(exception.Message, exception.InnerException)
+        {
+            this.StatusCode = statusCode;
+        }
+    }
+}
