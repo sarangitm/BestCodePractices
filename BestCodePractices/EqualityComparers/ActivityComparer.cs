a32f98ad344ea8b942083b862ff62e4e194ffee3
SarangK <sarang@itmystix.com>
2017-08-31 20:19:31 +0530

    Added Equality Comparer

---
+++ BestCodePractices/EqualityComparers/ActivityComparer.cs
@@ -0,0 +1,45 @@
+﻿using BestCodePractices.Models;
+using System.Collections.Generic;
+
+namespace BestCodePractices.EqualityComparers
+{
+    /// <summary>
+    /// ActivityComparer : comparing by identifier
+    /// </summary>
+    /// <seealso cref="System.Collections.Generic.IEqualityComparer{BestCodePractices.Models.Activity}" />
+    public class ActivityComparer : IEqualityComparer<Activity>
+    {
+        public bool Equals(Activity x, Activity y)
+        {
+            if (x.ActivityId == y.ActivityId)
+                return true;
+            return false;
+        }
+
+        public int GetHashCode(Activity obj)
+        {
+            return obj.ActivityId.GetHashCode();
+        }
+    }
+
+    /// <summary>
+    /// ActivityComparer: comapring multiple parameters
+    /// </summary>
+    /// <seealso cref="System.Collections.Generic.IEqualityComparer{BestCodePractices.Models.Activity}" />
+    public class MultiValueActivityComparer : IEqualityComparer<Activity>
+    {
+        public bool Equals(Activity x, Activity y)
+        {
+            if (x.ActivityId == y.ActivityId)
+                return true;
+            if (x.OwnerId == y.OwnerId)
+                return true;
+            return false;
+        }
+
+        public int GetHashCode(Activity obj)
+        {
+            return obj.ActivityId.GetHashCode();
+        }
+    }
+}
---
+++ BestCodePractices/Models/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestCodePractices.Models
+{
+    public class Activity
+    {
+        public int ActivityId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int OwnerId { get; set; }
+    }
+}
