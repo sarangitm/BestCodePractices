c1f1245d56d98eabd54d2a6a228c25ce5623d448
SarangK <sarang@itmystix.com>
2017-08-31 20:06:41 +0530

    Added Extension Methods for List & DateTime

---
+++ BestCodePractices/ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BestCodePractices.ExtensionMethods
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToUnixTimeStamp(this DateTime dateTime)
+        {
+            var unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var unixTimeStampInTicks = (dateTime - unixStart).Ticks;
+            var ticks = unixTimeStampInTicks / TimeSpan.TicksPerSecond;
+            return ticks.ToString();
+        }
+
+        public static DateTime GetStartDayOfWeek(this DateTime date, DayOfWeek startOfWeek)
+        {
+            int diff = date.DayOfWeek - startOfWeek;
+            if (diff < 0)
+            {
+                diff += 7;
+            }
+            return date.AddDays(-1 * diff).Date;
+        }
+
+        public static DateTime GetLastDayOfWeek(this DateTime date, DayOfWeek startOfWeek)
+        {
+            return date.GetStartDayOfWeek(startOfWeek).AddDays(6).Date;
+        }
+
+        public static DateTime ToTimeZone(this DateTime dateTime, TimeZoneInfo timeZoneInfo)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(dateTime.ToUniversalTime(), timeZoneInfo);
+        }
+
+        public static DateTime ToTimeZone(this DateTime dateTime, string timeZoneId)
+        {
+            TimeZoneInfo timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            return dateTime.ToUniversalTime().ToTimeZone(timeZoneInfo);
+        }
+    }
+}
---
+++ BestCodePractices/ExtensionMethods/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestCodePractices.ExtensionMethods
+{
+    public static class ListExtensions
+    {
+        public static bool IsFirst<T>(this List<T> list, T item)
+        {
+            return (list.IndexOf(item) == 0);
+        }
+
+        public static bool IsLast<T>(this List<T> list, T item)
+        {
+            return list.LastIndexOf(item).Equals(list.Count - 1);
+        }
+    }
+}
